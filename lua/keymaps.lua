3db276e26057d4d9ac8350003f812e22a2d6c2dc
Zach Ritter <zachritter@Zachs-MacBook-Pro.local>
2024-07-03 14:44:45 -0400

    init commit

---
+++ lua/keymaps.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>pf', '<C-^>', { desc = 'Go to [P]revious [F]ile', noremap = true, silent = true })
